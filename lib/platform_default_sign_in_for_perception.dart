bdffb7b48d118bfe5bae18578a9b560c7ca80235
Nick Meinhold <nick@enspyr.co>
2023-11-17 19:03:00 +1100

    export sign_in_screen

--- lib/platform_default_sign_in_for_perception.dart
+++ lib/platform_default_sign_in_for_perception.dart
@@ -4,6 +4,7 @@ import 'package:abstractions/beliefs.dart';
 import 'package:apple_sign_in_for_perception/apple_sign_in_for_perception.dart';
 
 export 'src/views/platform_default_sign_in_button.dart';
+export 'src/views/sign_in_screen.dart';
 
 void initializePlatformDefaultSignIn<S extends CoreBeliefs>({
   List<Consideration<S>>? considerOnProviderSignedIn,
