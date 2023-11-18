de1d7c4df76a36c5deb0372a245d1fbbfe835f10
Nick Meinhold <nick@enspyr.co>
2023-11-18 21:33:31 +1100

    Add type parameter when creating AppleSignInButton

--- lib/src/views/platform_default_sign_in_button.dart
+++ lib/src/views/platform_default_sign_in_button.dart
@@ -8,6 +8,6 @@ class PlatformDefaultSignInButton<T extends CoreBeliefs>
 
   @override
   Widget build(BuildContext context) {
-    return const AppleSignInButton();
+    return AppleSignInButton<T>();
   }
 }
